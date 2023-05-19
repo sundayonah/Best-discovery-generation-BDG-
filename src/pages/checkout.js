3a9fbf55bafb7242615b0298485120b24367b86e
sundayonah <sundayonah94@gmail.com>
2023-05-19 11:23:19 +0100

    update checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,5 +1,6 @@
 import Header from "@/componenrs/Header"
 import Image from "next/image"
+import checkOutBanner from "../images/book6.jpg"
 
 function Checkout() {
   return (
@@ -7,8 +8,21 @@ function Checkout() {
       <Header />
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* LEFT */}
+        <div className="flex-grow m-5 shadow-sm">
+          <img
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
+        </div>
+
+        {/* Right */}
         <div>
-          <Image />
+          <h1>Right</h1>
         </div>
       </main>
     </div>
