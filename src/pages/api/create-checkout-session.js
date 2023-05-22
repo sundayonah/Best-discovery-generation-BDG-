bcf5fededf68d8912e99df9cc6c70add243e1941
sundayonah <sundayonah94@gmail.com>
2023-05-22 09:17:47 +0100

    stripe-checkout-working now

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,38 +1,3 @@
-// require("dotenv").config()
-// const Stripe = require("stripe")
-
-// const stripe = Stripe(process.env.STRIPE_SECRETE_KEY)
-
-// export default async (req, res) => {
-// const { items, email } = req.body
-
-// console.log(items, email)
-// const transformedItems = items.map((item) => ({
-//   description: item.description,
-//   quantity: 1,
-//   price_data: {
-//     currency: "usd",
-//     unit_amount: item.price * 100,
-//     product_data: {
-//       name: item.title,
-//       images: [item.image],
-//     },
-//   },
-// }))
-
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: transformedItems,
-//     mode: "payment",
-//     success_url: `${process.env.NEXTAUTH_URL}/success`,
-//     cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
-//     metadata: {
-//       email,
-//       images: JSON.stringify(items.map((item) => item.image)),
-//     },
-//   })
-
-//   res.status(200).json({ id: session.id })
-// }
 // It was the same with me. You have the create-checkout-session.js
 // file that creates the checkout session. The data structure in there is not
 // suitable for the new Stripe API version. Read the documentation he is showing in the video.
@@ -42,35 +7,67 @@
 // This example sets up an endpoint using the Express framework.
 // Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 
-const express = require("express")
-const app = express()
-const stripe = require("stripe")(process.env.STRIPE_SECRETE_KEY)
+//THIS SNIPPET IS WORKING BUT ONLY FETCH ONE ITEM FROM THE CART
+// Import Stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
-export default async (req, res) => {
-  const { items, email } = req.body
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: items.title,
-            images: [items.image],
-          },
-          unit_amount: items.price * 100,
-        },
-        quantity: 1,
+// Create a function to create a checkout session
+const createCheckoutSession = async (req, res) => {
+   // Get the items and email from the request body
+   const { items, email } = req.body
+
+   const price = parseFloat(items[0].price)
+   // Validate the items and price
+   if (!Array.isArray(items) || items.length === 0) {
+      return res.status(400).json({ error: "Invalid items or price" })
+   }
+   console.log(items)
+   console.log(email)
+
+   // Convert the price to a number
+
+   // Validate the price
+   if (isNaN(price) || price <= 0) {
+      return res.status(400).json({ error: "Invalid price" })
+   }
+   console.log(price)
+
+   // Create a line item for each item
+   const lineItems = items.map((item) => ({
+      quantity: 1,
+      price_data: {
+         currency: "usd",
+         unit_amount: item.price * 100,
+         product_data: {
+            name: item.title,
+            images: [item.image],
+            description: item.description,
+         },
       },
-    ],
-    mode: "payment",
-    success_url: `${process.env.NEXTAUTH_URL}/success`,
-    cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
-    metadata: {
-      email,
-      images: JSON.stringify(items.map((item) => item.image)),
-    },
-  })
-  res.status(200).json({ id: session.id })
+   }))
+
+   // Create the checkout session
+   try {
+      const session = await stripe.checkout.sessions.create({
+         line_items: lineItems,
+         mode: "payment",
+         success_url: `${process.env.NEXTAUTH_URL}/success`,
+         cancel_url: `${process.env.NEXTAUTH_URL}/checkout`,
+         metadata: {
+            email,
+            images: JSON.stringify(items.map((item) => item.image)),
+         },
+      })
+
+      // Return the session ID
+      res.status(200).json({ id: session.id })
+   } catch (error) {
+      console.error(error)
+
+      // Return an error message
+      res.status(500).json({ error: "Failed to create checkout session" })
+   }
 }
 
-app.listen(3000, () => console.log(`Listening on port ${3000}!`))
+// Export the function
+export default createCheckoutSession
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -13,89 +13,101 @@ import { loadStripe } from "@stripe/stripe-js"
 
 const stripePromise = loadStripe(process.env.stripe_public_key)
 function Checkout() {
-  const items = useSelector(selectItems)
-  const { data: session } = useSession()
-  const total = useSelector(selectTotal)
+   const items = useSelector(selectItems)
+   const { data: session } = useSession()
+   const total = useSelector(selectTotal)
 
-  const createCheckoutSession = async () => {
-    const stripe = await stripePromise
+   const createCheckoutSession = async () => {
+      const stripe = await stripePromise
 
-    //call the backend ro create a checkout session...
-    const checkoutSession = await axios.post("api/create-checkout-session", {
-      items: items,
-      email: session.user.email,
-    })
-    console.log(checkoutSession)
+      //call the backend ro create a checkout session...
+      const checkoutSession = await axios.post("api/create-checkout-session", {
+         items: items,
+         email: session.user.email,
+      })
 
-    //Redirect user/customer to Stripe Checkout
-    const result = await stripe?.redirectToCheckout({
-      sessionId: checkoutSession.data.id,
-    })
+      console.log(items.price)
 
-    if (result?.error) alert(result.error.message)
-  }
+      //Redirect user/customer to Stripe Checkout
+      const result = await stripe.redirectToCheckout({
+         sessionId: checkoutSession.data.id,
+      })
+      console.log(checkoutSession.data)
 
-  return (
-    <div className="bg-gray-200">
-      <Header />
-      <main className="lg:flex max-w-screen-2xl mx-auto">
-        {/* LEFT */}
-        <div className="flex-grow m-5 shadow-sm">
-          <Image
-            src="https://links.papareact.com/ikj"
-            width={1020}
-            height={200}
-            objectfit="contain"
-          />
-          <div className="flex flex-col p-5 space-y-10 bg-white">
-            <h1 className="text-3xl border-b pb-4">
-              {items.length == 0
-                ? "Your Basket is empty"
-                : "Your Shopping Basket"}
-            </h1>
-            {items.map(
-              ({ id, title, price, description, category, image, rating }) => (
-                <CheckoutBook
-                  key={id}
-                  id={id}
-                  title={title}
-                  price={price}
-                  description={description}
-                  category={category}
-                  image={image}
-                  rating={rating}
-                />
-              )
-            )}
-          </div>
-        </div>
+      if (result.error) alert(result.error.message)
+   }
 
-        {/* Right */}
-        <div className="flex flex-col bg-white p-10 shadow-md">
-          {items.length > 0 && (
-            <>
-              <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items:){" "}
-                <span className="font-bold">
-                  <Currency quantity={total} currency="NGN" />
-                </span>
-              </h2>
-              <button
-                role="link"
-                onClick={createCheckoutSession}
-                disabled={!session}
-                className={`button mt-2 ${
-                  !session &&
-                  "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
-                }`}
-              >
-                {!session ? "Sign in to checkout" : "Proceed to Checkout"}
-              </button>
-            </>
-          )}
-        </div>
-      </main>
-    </div>
-  )
+   return (
+      <div className="bg-gray-200">
+         <Header />
+         <main className="lg:flex max-w-screen-2xl mx-auto">
+            {/* LEFT */}
+            <div className="flex-grow m-5 shadow-sm">
+               <Image
+                  src="https://links.papareact.com/ikj"
+                  width={1020}
+                  height={200}
+                  objectfit="contain"
+               />
+               <div className="flex flex-col p-5 space-y-10 bg-white">
+                  <h1 className="text-3xl border-b pb-4">
+                     {items.length == 0
+                        ? "Your Basket is empty"
+                        : "Your Shopping Basket"}
+                  </h1>
+                  {items.map(
+                     ({
+                        id,
+                        title,
+                        price,
+                        description,
+                        category,
+                        image,
+                        rating,
+                     }) => (
+                        <CheckoutBook
+                           key={id}
+                           id={id}
+                           title={title}
+                           price={price}
+                           description={description}
+                           category={category}
+                           image={image}
+                           rating={rating}
+                        />
+                     )
+                  )}
+               </div>
+            </div>
+
+            {/* Right */}
+            <div className="flex flex-col bg-white p-10 shadow-md">
+               {items.length > 0 && (
+                  <>
+                     <h2 className="whitespace-nowrap">
+                        Subtotal ({items.length} items:){" "}
+                        <span className="font-bold">
+                           <Currency quantity={total} currency="NGN" />
+                        </span>
+                     </h2>
+                     <button
+                        role="link"
+                        onClick={createCheckoutSession}
+                        disabled={!session}
+                        className={`button mt-2 ${
+                           !session &&
+                           "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
+                        }`}
+                     >
+                        {!session
+                           ? "Sign in to checkout"
+                           : "Proceed to Checkout"}
+                     </button>
+                  </>
+               )}
+            </div>
+         </main>
+      </div>
+   )
 }
 export default Checkout
