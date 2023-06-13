b3b9434de0a030fb9e9898ae62a475d0dba6addc
sundayonah <sundayonah94@gmail.com>
2023-06-13 14:05:45 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,3 @@
-// import Banner from "@/componenrs/Banner"
-// import Header from "@/componenrs/Header"
-// import Head from "next/head"
-// import BooksFeed from "@/componenrs/BooksFeed"
-// import { getSession } from "next-auth/react"
-// import { useState } from "react"
-// // import Books from "@/componenrs/Books"
-
-// export default function Home({ books }) {
-//    const [filteredBooks, setFilteredBooks] = useState(books)
-
-//    const handleSearch = (searchQuery) => {
-//       const filteredResults = books.filter((book) =>
-//          book.title.toLowerCase().includes(searchQuery.toLowerCase())
-//       )
-//       setFilteredBooks(filteredResults)
-//    }
-
-//    return (
-//       <div className="bg-gray-200 ">
-//          <Head>
-//             <title>Books</title>
-//          </Head>
-//          {/* HEADER */}
-//          <Header books={books} onSearch={handleSearch} />
-
-//          <main className="max-w-screen-2xl mx-auto ">
-//             {/* Banner */}
-//             <Banner />
-//             {/* ProductsFeed */}
-//             <BooksFeed books={filteredBooks} />
-//          </main>
-//       </div>
-//    )
-// }
-// export async function getServerSideProps(content) {
-//    const session = await getSession(content)
-
-//    //Fetch the books from the API
-//    const books = await fetch("https://fakestoreapi.com/products").then((res) =>
-//       res.json()
-//    )
-//    // .then((json) => console.log(json))
-//    return {
-//       props: {
-//          books,
-//          session,
-//       },
-//    }
-// }
-
 import Banner from "@/componenrs/Banner"
 import Header from "@/componenrs/Header"
 import Head from "next/head"
@@ -97,6 +46,9 @@ export async function getServerSideProps(content) {
    const books = await fetch("http://localhost:3000/api/books").then((res) =>
       res.json()
    )
+   //    const books = await fetch("https://fakestoreapi.com/products").then((res) =>
+   //    res.json()
+   // )
    // const data = await response.json()
    // setBooks(data)
    // console.error("Error fetching books:", books)
