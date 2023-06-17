7c2fd4fc43f7accbb0350ab96592cc32295760a3
sundayonah <sundayonah94@gmail.com>
2023-06-17 05:46:30 +0100

    fix error 500

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,243 +1,120 @@
-// import Header from "@/componenrs/Header"
-// // import checkOutBanner from "../images/book6.jpg"
-// import { useSelector } from "react-redux"
-// import { selectItems, selectTotal } from "@/slices/basketSlice"
-// import CheckoutBook from "@/componenrs/CheckoutBook"
-// // import Currency from "react-currency-formatter"
-// import { useSession } from "next-auth/react"
-// import axios from "axios"
-// import Image from "next/image"
-// import { loadStripe } from "@stripe/stripe-js"
-// import { FormattedNumber, IntlProvider } from "react-intl"
-// // const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
-// const stripePromise = loadStripe(process.env.stripe_public_key)
-// function Checkout() {
-//    const items = useSelector(selectItems)
-//    const { data: session } = useSession()
-//    const total = useSelector(selectTotal)
-//    //checkout.js
-//    //connect this to checkout button
-//    const createCheckoutSession = async () => {
-//       const stripe = await stripePromise
-
-
-//       //call the backend ro create a checkout session...
-//       const checkoutSession = await axios.post("api/create-checkout-session", {
-//          items: items,
-//          email: session.user.email,
-//       })
-//       //Redirect user/customer to Stripe Checkout
-//       const result = await stripe.redirectToCheckout({
-//          sessionId: checkoutSession.data.id,
-//       })
-// console.log(result)
-
-//       if (result.error) alert(result.error.message)
-//    }
-//    return (
-//       <div className="bg-gray-200">
-//          <Header />
-//          <main className="lg:flex max-w-screen-2xl mx-auto">
-//             {/* LEFT */}
-//             <div className="flex-grow m-5 shadow-sm">
-//                <Image
-//                   src="https://links.papareact.com/ikj"
-//                   width={1020}
-//                   height={200}
-//                   objectfit="contain"
-//                   alt="image"
-//                />
-//                <div className="flex flex-col p-5 space-y-10 bg-white">
-//                   <h1 className="text-3xl border-b pb-4">
-//                      {items.length == 0
-//                         ? "Your Basket is empty"
-//                         : "Your Shopping Basket"}
-//                   </h1>
-//                   {items.map(
-//                      ({
-//                         id,
-//                         title,
-//                         price,
-//                         description,
-//                         category,
-//                         image,
-//                         rating,
-//                      }) => (
-//                         <CheckoutBook
-//                         key={id}
-//                         id={id}
-//                         title={title}
-//                         price={price}
-//                         description={description}
-//                         category={category}
-//                         image={image}
-//                         rating={rating}
-//                         />
-//                         )
-//                         )}
-//                </div>
-//             </div>
-//             {/* Right */}
-//             <div className="flex flex-col bg-white p-10 shadow-md">
-//                {items.length > 0 && (
-//                   <>
-//                      <h2 className="whitespace-nowrap">
-//                         Subtotal ({items.length} items:){" "}
-//                         <span className="font-bold">
-//                            <IntlProvider locale="en-US">
-//                               <div>
-//                                  <FormattedNumber
-//                                     value={total}
-//                                     style="currency"
-//                                     currency="USD"
-//                                  />
-//                               </div>
-//                            </IntlProvider>
-//                            {/* <Currency quantity={total} currency="NGN" /> */}
-//                         </span>
-//                      </h2>
-//                      <button
-//                         role="link"
-//                         onClick={createCheckoutSession}
-//                         disabled={!session}
-//                         className={`button mt-2 ${
-//                            !session &&
-//                            "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
-//                         }`}
-//                      >
-//                         {!session
-//                            ? "Sign in to checkout"
-//                            : "Proceed to Checkout"}
-//                      </button>
-//                   </>
-//                )}
-//             </div>
-//          </main>
-//       </div>
-//    )
-// }
-// export default Checkout
-
-
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { loadStripe } from '@stripe/stripe-js';
-import { FormattedNumber, IntlProvider } from 'react-intl';
-import axios from 'axios';
-import Header from '@/componenrs/Header';
-import CheckoutBook from '@/componenrs/CheckoutBook';
-
-import { clearBasket, selectItems, selectTotal, storeCheckoutItems, getCheckoutItems, addToBasket } from '@/slices/basketSlice';
-// import { getCheckoutItems } from '@/utils/localStorage';
-
-const stripePromise = loadStripe(process.env.stripe_public_key);
-
+import Header from "@/componenrs/Header"
+// import checkOutBanner from "../images/book6.jpg"
+import { useSelector } from "react-redux"
+import { selectItems, selectTotal } from "@/slices/basketSlice"
+import CheckoutBook from "@/componenrs/CheckoutBook"
+// import Currency from "react-currency-formatter"
+import { useSession } from "next-auth/react"
+import axios from "axios"
+import Image from "next/image"
+import { loadStripe } from "@stripe/stripe-js"
+import { FormattedNumber, IntlProvider } from "react-intl"
+// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+const stripePromise = loadStripe(process.env.stripe_public_key)
 function Checkout() {
-  const items = useSelector(selectItems);
-  const { data: session } = useSession();
-  const total = useSelector(selectTotal);
-  const dispatch = useDispatch();
+   const items = useSelector(selectItems)
+   const { data: session } = useSession()
+   const total = useSelector(selectTotal)
+   //checkout.js
+   //connect this to checkout button
+   const createCheckoutSession = async () => {
+      const stripe = await stripePromise
 
-  // Store checkout items in local storage
-  const storeItems = () => {
-    storeCheckoutItems(items);
-  };
 
-  useEffect(() => {
-    // Retrieve checkout items from local storage
-    const persistedItems = getCheckoutItems();
-    dispatch(addToBasket(persistedItems));
+      //call the backend ro create a checkout session...
+      const checkoutSession = await axios.post("api/create-checkout-session", {
+         items: items,
+         email: session.user.email,
+      })
+      //Redirect user/customer to Stripe Checkout
+      const result = await stripe.redirectToCheckout({
+         sessionId: checkoutSession.data.id,
+      })
+console.log(result)
 
-    return () => {
-      // Clear basket and persist checkout items when component unmounts
-      dispatch(clearBasket());
-      storeItems();
-    };
-  }, []);
-
-  const createCheckoutSession = async () => {
-    const stripe = await stripePromise;
-
-    // Call the backend to create a checkout session
-    const response = await axios.post('/api/create-checkout-session', {
-      items: items,
-      email: session.user.email,
-    });
-
-    // Redirect user/customer to Stripe Checkout
-    const result = await stripe.redirectToCheckout({
-      sessionId: response.data.id,
-    });
-
-    if (result.error) {
-      alert(result.error.message);
-    }
-  };
-
-  return (
-    <div className="bg-gray-200">
-      <Header />
-      <main className="lg:flex max-w-screen-2xl mx-auto">
-        {/* LEFT */}
-        <div className="flex-grow m-5 shadow-sm">
-          <Image
-            src="https://links.papareact.com/ikj"
-            width={1020}
-            height={200}
-            objectFit="contain"
-            alt="image"
-          />
-          <div className="flex flex-col p-5 space-y-10 bg-white">
-            <h1 className="text-3xl border-b pb-4">
-              {items.length === 0 ? 'Your Basket is empty' : 'Your Shopping Basket'}
-            </h1>
-            {items.map(({ id, title, price, description, category, image, rating }) => (
-              <CheckoutBook
-                key={id}
-                id={id}
-                title={title}
-                price={price}
-                description={description}
-                category={category}
-                image={image}
-                rating={rating}
-              />
-            ))}
-          </div>
-        </div>
-        {/* Right */}
-        <div className="flex flex-col bg-white p-10 shadow-md">
-          {items.length > 0 && (
-            <>
-              <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items):{' '}
-                <span className="font-bold">
-                  <IntlProvider locale="en-US">
-                    <div>
-                      <FormattedNumber value={total} style="currency" currency="USD" />
-                    </div>
-                  </IntlProvider>
-                </span>
-              </h2>
-              <button
-                role="link"
-                onClick={createCheckoutSession}
-                disabled={!session}
-                className={`button mt-2 ${
-                  !session &&
-                  'from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed'
-                }`}
-              >
-                {!session ? 'Sign in to checkout' : 'Proceed to Checkout'}
-              </button>
-            </>
-          )}
-        </div>
-      </main>
-    </div>
-  );
+      if (result.error) alert(result.error.message)
+   }
+   return (
+      <div className="bg-gray-200">
+         <Header />
+         <main className="lg:flex max-w-screen-2xl mx-auto">
+            {/* LEFT */}
+            <div className="flex-grow m-5 shadow-sm">
+               <Image
+                  src="https://links.papareact.com/ikj"
+                  width={1020}
+                  height={200}
+                  objectfit="contain"
+                  alt="image"
+               />
+               <div className="flex flex-col p-5 space-y-10 bg-white">
+                  <h1 className="text-3xl border-b pb-4">
+                     {items.length == 0
+                        ? "Your Basket is empty"
+                        : "Your Shopping Basket"}
+                  </h1>
+                  {items.map(
+                     ({
+                        id,
+                        title,
+                        price,
+                        description,
+                        category,
+                        image,
+                        rating,
+                     }) => (
+                        <CheckoutBook
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                        rating={rating}
+                        />
+                        )
+                        )}
+               </div>
+            </div>
+            {/* Right */}
+            <div className="flex flex-col bg-white p-10 shadow-md">
+               {items.length > 0 && (
+                  <>
+                     <h2 className="whitespace-nowrap">
+                        Subtotal ({items.length} items:){" "}
+                        <span className="font-bold">
+                           <IntlProvider locale="en-US">
+                              <div>
+                                 <FormattedNumber
+                                    value={total}
+                                    style="currency"
+                                    currency="USD"
+                                 />
+                              </div>
+                           </IntlProvider>
+                           {/* <Currency quantity={total} currency="NGN" /> */}
+                        </span>
+                     </h2>
+                     <button
+                        role="link"
+                        onClick={createCheckoutSession}
+                        disabled={!session}
+                        className={`button mt-2 ${
+                           !session &&
+                           "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
+                        }`}
+                     >
+                        {!session
+                           ? "Sign in to checkout"
+                           : "Proceed to Checkout"}
+                     </button>
+                  </>
+               )}
+            </div>
+         </main>
+      </div>
+   )
 }
+export default Checkout
 
-export default Checkout;
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -1,131 +1,54 @@
-// import { createSlice } from "@reduxjs/toolkit"
-
-// const initialState = {
-//    items: [],
-// }
-
-// export const basketSlice = createSlice({
-//    name: "basket",
-//    initialState,
-//    reducers: {
-//       // addToBasket: (state, action) => {
-//       //    state.items = [...state.items, action.payload]
-//       // },
-
-//       addToBasket: (state, action) => {
-//          const { id, title, price, image } = action.payload
-      
-//          state.items = [...state.items, {
-//             id,
-//             title,
-//             price,
-//             image,
-//          }]
-//       },
-      
-//       removeFromBasket: (state, action) => {
-//          const index = state.items.findIndex(
-//             (basketItem) => basketItem.id === action.payload.id
-//          )
-
-//          let newBasket = [...state.items]
-//          if (index >= 0) {
-//             // the item exist in the basket
-//             newBasket.splice(index, 1)
-//          } else {
-//             console.warn(
-//                `Cant remove book (id: ${action.payload.id}) as its not found`
-//             )
-//          }
-//          state.items = newBasket
-//       },
-//    },
-// })
-
-// export const { addToBasket, removeFromBasket } = basketSlice.actions
-
-// // Selectors - This is how we pull information from the Global store slice
-// export const selectItems = (state) => state.basket.items
-// export const selectTotal = (state) =>
-//    state.basket.items.reduce((total, item) => total + item.price, 0)
-
-// export default basketSlice.reducer
-
-
-
-import { createSlice } from '@reduxjs/toolkit';
-// import { storeCheckoutItems } from '@/utils/localStorage';
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  items: [],
-};
+   items: [],
+}
 
 export const basketSlice = createSlice({
-  name: 'basket',
-  initialState,
-  reducers: {
-    addToBasket: (state, action) => {
-      const { id, title, price, image } = action.payload;
+   name: "basket",
+   initialState,
+   reducers: {
+      // addToBasket: (state, action) => {
+      //    state.items = [...state.items, action.payload]
+      // },
 
-      state.items = [
-        ...state.items,
-        {
-          id,
-          title,
-          price,
-          image,
-        },
-      ];
-    },
+      addToBasket: (state, action) => {
+         const { id, title, price, image } = action.payload
+      
+         state.items = [...state.items, {
+            id,
+            title,
+            price,
+            image,
+         }]
+      },
+      
+      removeFromBasket: (state, action) => {
+         const index = state.items.findIndex(
+            (basketItem) => basketItem.id === action.payload.id
+         )
 
-    removeFromBasket: (state, action) => {
-      const index = state.items.findIndex((basketItem) => basketItem.id === action.payload.id);
+         let newBasket = [...state.items]
+         if (index >= 0) {
+            // the item exist in the basket
+            newBasket.splice(index, 1)
+         } else {
+            console.warn(
+               `Cant remove book (id: ${action.payload.id}) as its not found`
+            )
+         }
+         state.items = newBasket
+      },
+   },
+})
 
-      let newBasket = [...state.items];
-      if (index >= 0) {
-        // The item exists in the basket
-        newBasket.splice(index, 1);
-      } else {
-        console.warn(`Can't remove book (id: ${action.payload.id}) as it's not found`);
-      }
-      state.items = newBasket;
-    },
-
-    clearBasket: (state) => {
-      state.items = [];
-    },
-  },
-});
-
-export const { addToBasket, removeFromBasket, clearBasket } = basketSlice.actions;
+export const { addToBasket, removeFromBasket } = basketSlice.actions
 
 // Selectors - This is how we pull information from the Global store slice
-export const selectItems = (state) => state.basket.items;
+export const selectItems = (state) => state.basket.items
 export const selectTotal = (state) =>
-  state.basket.items.reduce((total, item) => total + item.price, 0);
+   state.basket.items.reduce((total, item) => total + item.price, 0)
 
-export default basketSlice.reducer;
+export default basketSlice.reducer
 
-// Retrieve checkout items from local storage
-export const getCheckoutItems = () => {
-  try {
-    const serializedItems = localStorage.getItem('checkoutItems');
-    if (serializedItems === null) {
-      return [];
-    }
-    return JSON.parse(serializedItems);
-  } catch (error) {
-    console.log('Error retrieving checkout items from local storage:', error);
-    return [];
-  }
-};
 
-// Store checkout items in local storage
-export const storeCheckoutItems = (items) => {
-  try {
-    const serializedItems = JSON.stringify(items);
-    localStorage.setItem('checkoutItems', serializedItems);
-  } catch (error) {
-    console.log('Error storing checkout items in local storage:', error);
-  }
-};
