20a90e19901b646facea13871739aa332e634c5f
sundayonah <sundayonah94@gmail.com>
2023-07-03 17:46:02 +0100

    fix: delete external url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {}
 
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com", 'localhost'],
+    domains: ['localhost'],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
