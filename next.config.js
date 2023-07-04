d0f1d4a9ad54a6e13097afc7ae84401263740202
sundayonah <sundayonah94@gmail.com>
2023-07-04 16:07:06 +0100

    working on media querry

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {}
 module.exports = {
   images: {
     //this fix the image not showing
-    domains: ['augustin-obi.netlify.app', '64a327b650d4bf4ce07df652--augustin-obi.netlify.app/' ,'amazon-nextjs-zeta.vercel.app/'],
+    domains: ['augustin-obi.netlify.app'],
+    // domains: ['augustin-obi.netlify.app', '64a327b650d4bf4ce07df652--augustin-obi.netlify.app/'],
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
